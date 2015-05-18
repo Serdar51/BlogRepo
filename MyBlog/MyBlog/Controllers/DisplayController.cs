97c90d7ec3cbca6eaac9d0f952dd9b10c22fda5f
serdar51 <serdar_gucluer_051@hotmail.com>
2015-05-19 01:00:30 +0300

    Displaying details for a selected article.

---
+++ MyBlog/MyBlog/Controllers/DisplayController.cs
@@ -0,0 +1,31 @@
+﻿using MyBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyBlog.Controllers
+{
+    public class DisplayController : Controller
+    {
+        public ActionResult PostDetail(int id)
+        {
+            BlogContext db = new BlogContext();
+
+            Posts post = new Posts();
+
+            if (db.post.Count() >= id)
+            {
+                ViewData["Error"] = null;
+                post = db.post.Find(id);
+            }
+            else
+            {
+                ViewData["Error"] = "Cannot be found such a resource!!";
+            }
+            return View(post);
+        }
+
+    }
+}
