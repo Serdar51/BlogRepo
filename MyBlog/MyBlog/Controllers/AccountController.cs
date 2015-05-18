ae4ec7f74c95a5dccdd6cc6cd9d5aadc9f852b47
serdar51 <serdar_gucluer_051@hotmail.com>
2015-05-18 14:44:45 +0300

    Designing register page and taking a new user.

---
+++ MyBlog/MyBlog/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyBlog.Models;
+using System.Web.Security;
+
+namespace MyBlog.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(string Name, string EMailAddress, string Password)
+        {
+            Register register = new Register();
+            BlogContext db = new BlogContext();
+
+            if (ModelState.IsValid)
+            {
+                if (register.isValid(Name, EMailAddress))
+                {
+                    Users user = new Users();
+                    int num = db.user.Count();
+                    user.Id = num + 1;
+                    user.Password = Password;
+                    user.EMailAddress = EMailAddress;
+                    user.Name = Name;
+                    db.user.Add(user);
+                    db.SaveChanges();
+                    FormsAuthentication.SetAuthCookie(Name, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "There already exist an account that has the followings. Please enter different user name or email address!!");
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ MyBlog/MyBlog/Models/Register.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyBlog.Models
+{
+    public class Register
+    {
+        public bool isValid(string _Name, string _EMailAddress)
+        {
+            BlogContext db = new BlogContext();
+
+            int userForName = db.user.Count(x => x.EMailAddress.Equals(_Name));
+            int userForEMail = db.user.Count(x => x.EMailAddress.Equals(_EMailAddress));
+
+            // If there exists an account with the same user name it returns 1
+            if (userForName != 0 || userForEMail != 0) return false;
+            else return true;
+        }
+    }
+}
